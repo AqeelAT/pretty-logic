2f6b583e073b713ab64fdce5b8bf378e89481e84
Abdullah Alaqeel <a.alaqeel@sary.com>
2021-08-05 20:09:09 +0300

    chore: updated dist

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pretty-logic',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A pretty logic library',
